a057439a0ee5215a9d9ae72739c2c9d357590d9e
zenoamaro <zenoamaro@gmail.com>
2013-10-18 19:45:43 +0200

    Switched argument parsing to `node-optimist`

--- src/cli.js
+++ src/cli.js
@@ -1,53 +1,37 @@
 'use strict'
 
-var NetPipe = require('./netpipe')
+var optimist = require('optimist'),
+	NetPipe = require('./netpipe')
 
-var USAGE =
-	'Usage: netpipe [-h] pipe [arguments...]\n\n'+
-	'Options:\n'+
-	'  pipe           The pipe to read from or write to\n'+
-	'  arguments...   A list of arguments to pass to the pipe in the form\n'+
-	'                 arg=value another-arg=another-value\n'+
-	'  -h, --help     Shows this help screen\n'+
-	'  -v, --verbose  Enable verbose debugging output'
 
-function usage() {
-	console.error(USAGE)
-	process.exit(1)
+var usage = 'Pipes STDIN/STDOUT to a remote plug.\n' +
+            'Usage: netpipe [options] plug_url [arguments...]'
+
+var options = {
+	'verbose': { alias:'v', boolean:true,
+	             describe:'Show verbose and debugging output' },
+	'help':    { alias:'h', boolean:true,
+	             describe:'Show this help screen' },
 }
 
-// Ignore Node and script name
-var args = process.argv.slice(2),
-	options = {},
-	remaining = []
-
-// Not using a forEach as we're going to
-// alter the counter during looping
-for (var i=0, l=args.length; i<l; i++) {
-	var arg = args[i]
-	// Not an option, we'll store and parse this later
-	if (arg[0] != '-')
-		remaining.push(arg)
-	// Starts with `-` so treat it as an option
-	else switch (arg) {
-		// These are bools and we can just switch them
-		case '-v':
-		case '--verbose':
-			options['verbose'] = true
-			break
-		// Display usage when requested or specifying an invalid option
-		case '-h':
-		case '--help':
-		default:
-			usage()
-	}
+function checkArguments(argv) {
+	if (argv.help)
+		throw null
+	if (argv._.length < 1)
+		throw 'ERROR: Please provide the URL for the pipe.'
 }
 
-// The pipe resource is required
-if (!remaining.length)
-	usage()
 
-var pipe = remaining[0],
-	args = remaining.slice(1)
+var argv = optimist
+	.usage(usage)
+	.options(options)
+	.check(checkArguments)
+	.argv
 
-var netpipe = new NetPipe(pipe, args, options)
+
+var pipe = argv._[0], // First
+	args = argv._.slice(1) // Rest
+
+var netpipe = new NetPipe(pipe, args, {
+	verbose: argv.verbose,
+})
